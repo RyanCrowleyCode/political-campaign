6803d10750bb8d4ba48efc9270f93a3bd88b4491
Ryan Crowley <ryancrowleycode@gmail.com>
2019-10-11 15:15:39 -0500

    copied question in from the chapter and formatted comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,38 @@
-console.log("everybody clap your hands!")
+// Challenge: Political Campaign
+// Challenges are optional exercises that you should attempt only if you've completed the practice exercises and understand the concepts.
+
+// You have volunteered your time to a local political candidate, Elizabeth Sanger, who wants to become a US representative in Congress for your district. Unfortunately, the team discovered that you're a software developer, so they have begged you to build an application that lets them track volunteers, and store information about Elizabeth and her campaign.
+
+// Your job is to define the different objects and arrays, their structure, and the corresponding properties for each, to represent the following information about Elizabeth's campaign.
+
+// Her congressional district (you can use yours here)
+
+// Her platform statements for the following issues.
+//// 1. Taxes
+//// 2. Jobs
+//// 3. Infrastructure
+//// 4. Health care
+//// 5. Crime and enforcement
+
+// URL for donation form
+
+// Calendar of events
+
+// Volunteer information
+//// 1. Name
+//// 2. Address
+//// 3. Email
+//// 4. Phone number
+//// 5. Availability
+//// 6. What activities each one is willing to do (e.g. answering phone calls, taking polls, etc.)
+
+// Biography
+
+// Image gallery
+//// 1. Head shot
+//// 2. Picture of family
+//// 3. Picture of constituents
+
+// Mission statement
+
+// URL for registering to vote
